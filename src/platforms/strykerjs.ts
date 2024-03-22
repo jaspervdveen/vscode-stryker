08a36906282336b5683d217f867562993865fec8
Jasper van der Veen <jasper.vanderveen99@gmail.com>
2024-03-22 11:18:45 +0100

    feat(instrumentation): Show progress in editor

--- src/platforms/strykerjs.ts
+++ src/platforms/strykerjs.ts
@@ -3,21 +3,29 @@ import { Platform } from "./platform.js";
 import { errorNotification } from "../utils/reporter.js";
 import { FileUtil } from "../utils/file.js";
 import { MutationTestResult } from "../api/schema.js";
+import { ProgressLocation, window } from "vscode";
 
 export class StrykerJs implements Platform {
 
+    // temp path to the unpublished stryker executable while in development
+    // TODO: Make this configurable/autodetect
     executable = '/home/jasper/repos/stryker-js/packages/core/bin/stryker.js';
 
     async instrumentationRun(): Promise<MutationTestResult> {
-        try {
-            const command = `${this.executable} run --instrumentRunOnly`;
-            await executeCommand(command);
+        return await window.withProgress({
+            location: ProgressLocation.Window,
+            title: 'Stryker: Instrumenting code',
+            cancellable: false
+        }, async () => {
+            try {
+                const command = `${this.executable} run --instrumentRunOnly`;
+                await executeCommand(command);
 
-            return await FileUtil.readMutationReport(FileUtil.getMutationReportUri());
-        } catch (error) {
-            console.log('Error running mutation testing:', error);
-            errorNotification('Error running mutation testing');
-            throw new Error('Error running mutation testing');
-        }
+                return await FileUtil.readMutationReport(FileUtil.getMutationReportUri());
+            } catch (error) {
+                errorNotification('Stryker: Error running instrumentation');
+                throw new Error('Stryker: Error running instrumentation');
+            }
+        });
     }
 }
