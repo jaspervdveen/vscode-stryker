f45bd2084a24ed7e8c448bbb5e7ca28549a4e5fa
Jasper van der Veen <jasper.vanderveen99@gmail.com>
2024-03-25 13:36:08 +0100

    refactor(platform): Make use of abstract class with default implementation

--- src/platforms/platform.ts
+++ src/platforms/platform.ts
@@ -1,6 +1,30 @@
+import { errorNotification } from "../utils/reporter.js";
+import { FileUtil } from "../utils/file.js";
+import { ProgressLocation, window } from "vscode";
 import { MutationTestResult } from "mutation-testing-report-schema";
+import { config } from "../config.js";
+import { spawnSync } from "child_process";
 
-export interface Platform {
-    instrumentationRun(): Promise<MutationTestResult>;
+export abstract class Platform {
+    executable: string;
+
+    constructor(executable: string) {
+        this.executable = executable;
+    }
+
+    public async instrumentationRun(args?: string[]): Promise<MutationTestResult> {
+        return await window.withProgress({
+            location: ProgressLocation.Window,
+            title: config.messages.instrumentationRunning,
+        }, async () => {
+            try {
+                spawnSync(this.executable, args, { cwd: config.app.currentWorkingDirectory });
+                return await FileUtil.readMutationReport(FileUtil.getMutationReportUri());
+            } catch (error) {
+                errorNotification(config.errors.instrumentationFailed);
+                throw new Error(config.errors.instrumentationFailed);
+            }
+        });
+    }
 }
 
--- src/platforms/strykerjs.ts
+++ src/platforms/strykerjs.ts
@@ -1,33 +1,17 @@
-import { executeCommand } from "../utils/executor.js";
 import { Platform } from "./platform.js";
-import { errorNotification } from "../utils/reporter.js";
-import { FileUtil } from "../utils/file.js";
-import { ProgressLocation, window } from "vscode";
 import { MutationTestResult } from "mutation-testing-report-schema";
-import { config } from "../config.js";
 
-export class StrykerJs implements Platform {
+export class StrykerJs extends Platform {
 
-    // temp path to the unpublished stryker executable while in development
-    // TODO: Make this configurable/autodetect
-    executable = '/home/jasper/repos/stryker-js/packages/core/bin/stryker.js';
+    constructor() {
+        // temp path to the unpublished stryker executable while in development
+        // TODO: Make this configurable/autodetect
+        super('/home/jasper/repos/stryker-js/packages/core/bin/stryker.js');
+    }
 
     async instrumentationRun(): Promise<MutationTestResult> {
-        return await window.withProgress({
-            location: ProgressLocation.Window,
-            title: config.messages.instrumentationRunning,
-            cancellable: false
-        }, async () => {
-            try {
-                const command = `${this.executable} run --instrumentRunOnly --checkers ""` + 
-                ` --buildCommand "" --plugins "" --testRunner "" --logLevel off --reporters json`;
-                await executeCommand(command);
+        let args: string[] = ['run', '--checkers ""', '--buildCommand ""', '--plugins ""', '--testRunner ""', '--logLevel off', '--reporters json'];
 
-                return await FileUtil.readMutationReport(FileUtil.getMutationReportUri());
-            } catch (error) {
-                errorNotification(config.errors.instrumentationFailed);
-                throw new Error(config.errors.instrumentationFailed);
-            }
-        });
+        return super.instrumentationRun(args);
     }
 }
