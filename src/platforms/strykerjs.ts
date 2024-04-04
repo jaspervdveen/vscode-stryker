6fa496201f30f55750a363888e2d62f56f140630
Jasper van der Veen <jasper.vanderveen99@gmail.com>
2024-04-04 17:38:37 +0200

    wip: Implement initial JSON RPC messaging

--- src/platforms/strykerjs.ts
+++ src/platforms/strykerjs.ts
@@ -5,19 +5,44 @@ import { config } from "../config.js";
 import { ChildProcess, ChildProcessWithoutNullStreams, spawn, spawnSync } from "child_process";
 import { fileUtils } from "../utils/file-utils.js";
 import { reporterUtils } from "../utils/reporter-utils.js";
+import { JSONRPCClient, JSONRPCRequest, JSONRPCResponse, TypedJSONRPCClient } from "json-rpc-2.0";
+
+type Methods = {
+    instrument(params: { globPatterns?: string[] }): string;
+};
 
 export class StrykerJs implements Platform {
 
+    private rpcClient: TypedJSONRPCClient<Methods>;
     private server: ChildProcessWithoutNullStreams;
 
     constructor() {
         // temp path to the unpublished stryker executable while in development
         // TODO: Make this configurable/autodetect
         const executable = '/home/jasper/repos/stryker-js/packages/core/bin/stryker-server.js';
+
         this.server = spawn(executable, { cwd: config.app.currentWorkingDirectory });
 
-        this.server.stdout.on('data', (data) => {
-            console.log(`stdout: ${data}`);
+        this.server.stdout.on('data', (data: Buffer | string) => {
+            let response: JSONRPCResponse | undefined;
+
+            try {
+                response = JSON.parse(data.toString());
+            } catch (error) {
+                console.log('Error parsing JSON: ', error);
+            }
+
+            if (response) {
+                this.rpcClient.receive(response);
+            }
+        });
+
+        this.server.on('exit', (code) => {
+            this.rpcClient.rejectAllPendingRequests("Server exited with code " + code);
+        });
+
+        this.rpcClient = new JSONRPCClient((jsonRpcRequest: JSONRPCRequest) => {
+            this.server.stdin.write(JSON.stringify(jsonRpcRequest) + '\n');
         });
     }
 
@@ -27,10 +52,9 @@ export class StrykerJs implements Platform {
             title: config.messages.instrumentationRunning,
         }, async () => {
             try {
-                this.server.stdin.write('run instrumentation\n');
+                const result = await this.rpcClient.request('instrument', { globPatterns: globPatterns });
 
-                // spawnSync(this.executable, { cwd: config.app.currentWorkingDirectory });
-                return await fileUtils.readMutationReport(fileUtils.getMutationReportUri());
+                return {} as MutationTestResult;
             } catch (error) {
                 reporterUtils.errorNotification(config.errors.instrumentationFailed);
                 throw new Error(config.errors.instrumentationFailed);
@@ -55,8 +79,6 @@ export class StrykerJs implements Platform {
         }, async () => {
             try {
                 throw new Error('Not implemented');
-                // spawnSync(this.executable, args, { cwd: config.app.currentWorkingDirectory });
-                return await fileUtils.readMutationReport(fileUtils.getMutationReportUri());
             } catch (error) {
                 reporterUtils.errorNotification(config.errors.mutationTestingFailed);
                 throw new Error(config.errors.mutationTestingFailed);
