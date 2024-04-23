e651e01e77e589c0238db452908e257bf2cf1c63
Jasper van der Veen <jasper.vanderveen99@gmail.com>
2024-04-23 14:14:47 +0200

    fix(tests): Unit tests require opened workspace folder

--- .vscode-test.mjs
+++ .vscode-test.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from '@vscode/test-cli';
 
 export default defineConfig({
 	files: 'out/test/**/*.spec.js',
-	workspaceFolder: './test/testWorkspace',
+	workspaceFolder: './test/resources/empty-workspace',
 	mocha: {
 		require: ["./out/test/setup.js"]
 	}
--- test/unit/utils/path-utils.spec.ts
+++ test/unit/utils/path-utils.spec.ts
@@ -1,30 +1,29 @@
 import { assert } from 'chai';
-import { describe, it } from 'mocha';
 
 import { pathUtils } from '../../../src/utils/path-utils';
 
-describe('pathUtils', () => {
-  describe('filterCoveredPatterns', () => {
-    it('should filter out covered glob patterns', () => {
+suite('pathUtils', () => {
+  suite('filterCoveredPatterns', () => {
+    test('should filter out covered glob patterns', () => {
       const globPatterns: string[] = ['src/**/*.ts', 'src/index.ts', 'src/app.ts', 'src/components/**/*.ts', 'dist/**/*.js'];
       const filteredPatterns = pathUtils.filterCoveredPatterns(globPatterns);
       const expectedResults: string[] = ['src/**/*.ts', 'dist/**/*.js'];
       assert.deepEqual(filteredPatterns, expectedResults);
     });
 
-    it('should return empty array if no glob patterns are provided', () => {
+    test('should return empty array if no glob patterns are provided', () => {
       const globPatterns: string[] = [];
       const filteredPatterns = pathUtils.filterCoveredPatterns(globPatterns);
       assert.deepEqual(filteredPatterns, []);
     });
 
-    it('should return the same array if all patterns are unique', () => {
+    test('should return the same array if all patterns are unique', () => {
       const globPatterns = ['*.txt', 'src/*.js', '**/*.json'];
       const filteredPatterns = pathUtils.filterCoveredPatterns(globPatterns);
       assert.deepEqual(filteredPatterns, globPatterns);
     });
 
-    it('should handle a single pattern array', () => {
+    test('should handle a single pattern array', () => {
       const globPatterns = ['*.txt'];
       const filteredPatterns = pathUtils.filterCoveredPatterns(globPatterns);
       assert.deepEqual(filteredPatterns, globPatterns);
